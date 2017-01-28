108d4a020d19403ba7af4033f7fca12fdce24330
Andriy Zasypkin <AndriyZasypkin@gmail.com>
2017-01-27 21:05:23 -0500

    Player files have been added

---
+++ include/tiles/person.hpp
@@ -0,0 +1,39 @@
+///////////////////////////////////////////////////////////////////////
+// person.hpp
+// This file is part of Return of the Horsemen: A Tale of Calamity in a Perfect
+// World
+//
+// Copyright (C) 2017 - Revelations Gaming
+//
+// Return of the Horsemen: A Tale of Calamity in a Perfect World is free
+// software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// Return of the Horsemen: A Tale of Calamity in a Perfect World is distributed
+// in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Return of the Horsemen: A Tale of Calamity in a Perfect World. If
+// not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////
+
+#ifndef PERSON_HPP
+#define PERSON_HPP
+
+#include "tiles/veclocation.hpp"
+
+class Person : public sf::Drawable, public VecLocation {
+
+public:
+  //Draw tile
+  void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+
+}
+
+#endif
+
